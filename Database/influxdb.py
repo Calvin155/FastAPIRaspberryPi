d9b7f076010f763dec22908369adcc21aa3286fd
calvin <calvinlynch181@outlook.com>
2025-03-20 13:57:52 +0000

    influx - Read only token

--- Database/influxdb.py
+++ Database/influxdb.py
@@ -3,7 +3,9 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime, timedelta
 import logging
 
-INFLUXDB_URL = "http://172.17.0.2:8086"
+# Docker container address - Internal
+INFLUXDB_URL = "http://172.17.0.3:8086"
+# Read Only Token
 INFLUXDB_TOKEN="TulR-Qf6uq23i7Ny0h1MmRF3nxuliy8OXU_zyYS4KqfjGRSaQLugwfH_Th7t_jiij8x2wQBtSExOvWvlfVEqJg=="
 INFLUXDB_ORG = "AQI"
 INFLUXDB_BUCKET = "AQIMetrics"
