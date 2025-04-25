bc0c40dba57ad763d8891c9b45bd62fc094f282f
Calvin155 <calvinlynch181@outlook.com>
2025-04-25 17:05:30 +0100

    test

--- controllers/CO2_Temp_Humidity.py
+++ controllers/CO2_Temp_Humidity.py
@@ -10,9 +10,11 @@ class CO2TempHumidityController:
         self.setup_routes()
 
     def setup_routes(self):
-        self.router.add_api_route("/aqi_co2_data",self.read_co2_data,methods=["GET"],)
-        self.router.add_api_route("/aqi_co2_percentage_data",self.read_co2_percentage_data,methods=["GET"],)
-        self.router.add_api_route("/aqi_co2_ppm_data",self.read_co2_ppm_data,methods=["GET"],)
+        self.router.add_api_route("/aqi_co2_data",self.read_co2_data,methods=["GET"])
+        self.router.add_api_route("/aqi_co2_temp_humidity_data", self.read_co2_temp_humidity_data, methods=["GET"])
+        self.router.add_api_route("/aqi_co2_data",self.read_co2_data,methods=["GET"])
+        self.router.add_api_route("/aqi_co2_percentage_data",self.read_co2_percentage_data,methods=["GET"])
+        self.router.add_api_route("/aqi_co2_ppm_data",self.read_co2_ppm_data,methods=["GET"])
 
     async def read_co2_data(self) -> List[AQIData]:
         influx_db = InfluxDB()
@@ -25,6 +27,23 @@ class CO2TempHumidityController:
             ]
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
+        
+
+    async def read_co2_temp_humidity_data(self) -> List[AQIData]:
+        influx_db = InfluxDB()
+        try:
+            data = influx_db.get_co2_data()
+            aqi_list = []
+            
+            for table in data:
+                for record in table.records:
+                    aqi_list.append(AQIData(time=record.get_time().isoformat(), value=record.get_value()))
+            
+            return aqi_list
+        
+        except Exception as e:
+            print(f"Error fetching AQI data: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
 
     async def read_co2_percentage_data(self,*,start: datetime = Query(...,description="ISO8601",),end: Optional[datetime] = Query(None,description="ISO8601",),) -> List[AQIData]:
         influx_db = InfluxDB()
